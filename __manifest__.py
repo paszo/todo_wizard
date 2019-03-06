979915fd7ce2778959238158d34c06ac0d61f00f
Paszo <paszo2008@gmail.com>
2019-03-06 21:01:56 +0100

    [ADD] Added a wizard - simple but working

---
+++ __manifest__.py
@@ -0,0 +1,6 @@
+{'name': 'To-do Tasks Management Assistand',
+ 'description': 'Mass edit your To-Do backlog',
+ 'author': 'Daniel Reis',
+ 'depends': ['todo_app'],
+ 'data': ['views/todo_wizard_view.xml'],
+ }
