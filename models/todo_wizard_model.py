b2da5502afe0ee95a64973704b924f89a62a6d93
Paszo <paszo2008@gmail.com>
2019-03-06 21:40:37 +0100

    [ADD] Add mass update method

--- models/todo_wizard_model.py
+++ models/todo_wizard_model.py
@@ -1,4 +1,8 @@
 from odoo import models, fields, api
+from odoo import exceptions
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class TodoWizard(models.TransientModel):
     _name = 'todo.wizard'
@@ -16,3 +20,22 @@ class TodoWizard(models.TransientModel):
         defaults = super(TodoWizard, self).default_get(field_names)
         defaults['task_ids'] = self.env.context['active_ids']
         return defaults
+
+    @api.multi
+    def do_mass_update(self):
+        self.ensure_one()
+        if not (self.new_deadline or self.new_user_id):
+            raise excepitons.ValidationError('No data to update!')
+        # Logging debug messages
+        _logger.debug(
+            'Mass update on Todo Tasks %s',
+            self.task_ids.ids)
+        vals = {}
+        if self.new_deadline:
+            vals['date_deadline'] = self.new_deadline
+        if self.new_user_id:
+            vals['user_id'] = self.new_user_id.id
+        # Mass write values on all selected tasks
+        if vals:
+            self.task_ids.write(vals)
+        return True
